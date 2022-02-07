74089b4476b5576d7f65c20b2626cc162371cbb9
Minji Seo <63698668+ireneeming@users.noreply.github.com>
2022-02-07 22:46:12 +0900

    Update firebase.js

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -7,12 +7,12 @@ export const apiKey = "AIzaSyB3Nq15ShYhqhyfwoPzt-xalpzWjOxtlaU";
 
 const firebaseConfig = {
   apiKey: apiKey,
-  authDomain: "fakeinsta-29b6b.firebaseapp.com",
-  projectId: "fakeinsta-29b6b",
-  storageBucket: "fakeinsta-29b6b.appspot.com",
-  messagingSenderId: "516642740352",
-  appId: "1:516642740352:web:43da819bc3ef4b5a68ee65",
-  measurementId: "G-QH34KHFLMK"
+  authDomain: "*",
+  projectId: "*",
+  storageBucket: "*",
+  messagingSenderId: "*",
+  appId: "*",
+  measurementId: "*"
 };
 
 // Initialize Firebase
